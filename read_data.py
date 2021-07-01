113627181fe6f0706087a217a97c48552e891636
zoraulia <56012719+zoraulia@users.noreply.github.com>
2021-07-01 16:35:34 +0700

    revise read data file

--- read_data.py
+++ read_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import joblib
 
 PATH='susenas12/sn12_ki.csv'
 
@@ -11,11 +10,5 @@ def read_data(file_directory):
         csv file location
     '''
     data = pd.read_csv(file_directory).copy()
-    
-    joblib.dump(data,
-                "./output/sus12_ki.pkl")
-    
-    print("-----------------DONE read data-----------------")   
 
-if __name__ == '__main__':
-    read_data(PATH)
+    return data
